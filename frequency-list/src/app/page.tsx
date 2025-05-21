1ffa87a4fd534d0cef192d14f274372cc0fea3ed
Arnaldo Liro <dinholiro@gmail.com>
2025-05-21 13:56:51 -0300

    feat: creating the component that will store the event cards

--- frequency-list/src/app/page.tsx
+++ frequency-list/src/app/page.tsx
@@ -1,9 +1,11 @@
 import MainHeader from "@/components/MainHeader";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
     <div className="mt-10">
       <MainHeader />
+      <Card />
     </div>
   );
 }
