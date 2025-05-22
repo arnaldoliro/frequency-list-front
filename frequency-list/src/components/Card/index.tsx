1a79c714da6867949ee7b08f55b90410584bae56
Arnaldo Liro <dinholiro@gmail.com>
2025-05-22 10:44:30 -0300

    feat: animating modal entrance

--- frequency-list/src/components/Card/index.tsx
+++ frequency-list/src/components/Card/index.tsx
@@ -13,14 +13,12 @@ type CardProps = {
 };
 
 export default function Card({ nome, data, hora, organizado, status, local }: CardProps) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     
-    const openModal = () => setIsModalOpen(true);
-    const closeModal = () => setIsModalOpen(false);
 
     return(
         <>
-            <div className="bg-[#fff] w-90 h-auto mt-3 rounded-md p-2 hover:shadow-lg transition-all duration-300 hover:scale-107" onClick={openModal}>
+            <div className="bg-[#fff] w-90 h-auto mt-3 rounded-md p-2 hover:shadow-lg transition-all duration-300 hover:scale-107" onClick={() => setIsOpen(true)}>
                 <div className="flex justify-between items-center">
                     <h1 className="text-lg font-semibold mr-10">{nome}</h1>
                     <h1 className={`px-1 rounded-lg w-max h-max text-xs flex items-center font-normal
@@ -34,7 +32,12 @@ export default function Card({ nome, data, hora, organizado, status, local }: Ca
                 <h1 className="text-sm font-semibold text-[#494949]"> Dia {data} às {hora}</h1>
                 <button className="mt-3 bg-blue-300 cursor-pointer rounded-md px-1 text-sm hover:shadow-md hover:bg-blue-400 transition-all duration-300 hover:scale-107">Acessar</button>
             </div>
-            <Modal isOpen={isModalOpen} onClose={closeModal} />
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                <div className="py-4 px-6">
+                    <h1>{nome}</h1>
+                    <p>Mais detalhes do evento...</p>
+                </div>
+            </Modal>
         </>
     )
 }
